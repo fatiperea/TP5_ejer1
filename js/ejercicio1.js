aa3f4732a878bf73df6939a2f065afdecb46a0ab
fatiperea <fatimaperea3pppp0@gmail.com>
2023-11-23 13:03:53 -0300

    creo fc recibeNumero para guardar el num ingresado x el usuario

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -4,7 +4,25 @@ const iniciaJuego=() =>{
    console.log(aleatorio);
 }
 
+const recibeNumero= (e) => {
+    e.preventDefault();
+    const numIngresado=document.querySelector(".form-control").value;
+    console.log(numIngresado);
+
+    inputNumero.reset();
+
+}
+
+const comparaNumero=()=>{
+
+}
+
 const btnJuego = document.getElementById("juego");
 console.log(btnJuego);
 
 btnJuego.addEventListener("click",iniciaJuego);
+
+const inputNumero= document.querySelector("form");
+console.log(inputNumero);
+
+inputNumero.addEventListener('submit',recibeNumero);
