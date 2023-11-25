eadd220ebafb1bd683d5527890302e63a853cf24
fatiperea <fatimaperea3pppp0@gmail.com>
2023-11-24 23:08:24 -0300

    se corrije la comparacion en fc recibe numero, ya q sin parsear no se aplicaba la =dad estricta

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,33 +1,32 @@
-const iniciaJuego=() =>{
+const iniciaJuego = () => {
+  const aleatorio = Math.floor(Math.random() * 100 + 1);
+  console.log("aleatorio: " + aleatorio);
+  return aleatorio;
+};
 
-   const aleatorio= Math.floor(Math.random() * 100 + 1);
-   console.log("aleatorio: "+aleatorio);
-   return aleatorio;
-}
+const recibeNumero = (e) => {
+  e.preventDefault();
+  const numIngresado = parseInt(document.querySelector(".form-control").value);
+  console.log("ingresado " + numIngresado);
+  //console.log("igualdad estricta: ing vs mag",numIngresado===magico);
 
-const recibeNumero= (e) => {
-    e.preventDefault();
-    const numIngresado=parseInt(document.querySelector(".form-control").value);
-    console.log("ingresado "+numIngresado);
-    console.log("igualdad estricta: ing vs mag",numIngresado===magico);
+  if (numIngresado === magico) alert("ADIVINASTE EL NUMERO MAGICO!!");
+  else if (numIngresado > magico)
+    alert("NO ADIVINASTE...Tu numero es mayor que el numero magico");
+  else if (numIngresado < magico)
+    alert("NO ADIVINASTE...Tu numero es menor que el numero magico");
 
-    if(numIngresado===magico) alert("ADIVINASTE EL NUMERO MAGICO!!");
-    else if(numIngresado>magico) alert("NO ADIVINASTE...Tu numero es mayor que el numero magico");
-    else if(numIngresado<magico) alert("NO ADIVINASTE...Tu numero es menor que el numero magico"); 
-
-    inputNumero.reset();
-
-}
+  inputNumero.reset();
+};
 
 const btnJuego = document.getElementById("juego");
 //console.log(btnJuego);
 
- btnJuego.addEventListener("click",iniciaJuego); 
+btnJuego.addEventListener("click", iniciaJuego);
 const magico = parseInt(iniciaJuego());
-console.log("guardo magico",magico)
-const inputNumero= document.querySelector("form");
+console.log("guardo magico", magico);
+const inputNumero = document.querySelector("form");
 //console.log(inputNumero);
 
-inputNumero.addEventListener('submit',recibeNumero);
+inputNumero.addEventListener("submit", recibeNumero);
 //const recibido=recibeNumero();
-
