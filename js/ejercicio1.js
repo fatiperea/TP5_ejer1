27a3e5d9a8167c50f8ad1124754602f31e3dcacc
fatiperea <fatimaperea3pppp0@gmail.com>
2023-11-24 23:04:45 -0300

    se corrije la comparacion en fc recibe numero, ya q sin parsear no se aplicaba la =dad estricta

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,28 +1,33 @@
 const iniciaJuego=() =>{
 
    const aleatorio= Math.floor(Math.random() * 100 + 1);
-   console.log(aleatorio);
+   console.log("aleatorio: "+aleatorio);
+   return aleatorio;
 }
 
 const recibeNumero= (e) => {
     e.preventDefault();
-    const numIngresado=document.querySelector(".form-control").value;
-    console.log(numIngresado);
+    const numIngresado=parseInt(document.querySelector(".form-control").value);
+    console.log("ingresado "+numIngresado);
+    console.log("igualdad estricta: ing vs mag",numIngresado===magico);
+
+    if(numIngresado===magico) alert("ADIVINASTE EL NUMERO MAGICO!!");
+    else if(numIngresado>magico) alert("NO ADIVINASTE...Tu numero es mayor que el numero magico");
+    else if(numIngresado<magico) alert("NO ADIVINASTE...Tu numero es menor que el numero magico"); 
 
     inputNumero.reset();
 
 }
 
-const comparaNumero=()=>{
-
-}
-
 const btnJuego = document.getElementById("juego");
-console.log(btnJuego);
+//console.log(btnJuego);
 
-btnJuego.addEventListener("click",iniciaJuego);
-
+ btnJuego.addEventListener("click",iniciaJuego); 
+const magico = parseInt(iniciaJuego());
+console.log("guardo magico",magico)
 const inputNumero= document.querySelector("form");
-console.log(inputNumero);
+//console.log(inputNumero);
 
 inputNumero.addEventListener('submit',recibeNumero);
+//const recibido=recibeNumero();
+
